a09a02d8556f7ca076b6e80c26cd7544850555e2
Dev Govindji <friendsofdev94@gmail.com>
2019-10-30 13:25:13 -0500

    initializes repository class

---
+++ app/kudo/service.py
@@ -0,0 +1,3 @@
+from ..repository import Repository
+from ..repository.mongo import MongoRespository
+from .schema import KudoSchema
--- app/repository/mongo.py
+++ app/repository/mongo.py
@@ -1,6 +1,7 @@
 import os
 from pymongo import MongoClient
 
+
 COLLECTION_NAME = 'kudos'
 
 
