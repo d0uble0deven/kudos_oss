cd25aed018ab238de190ed64e68b3e7a9c7b6f97
Dev Govindji <friendsofdev94@gmail.com>
2019-11-04 15:44:59 -0600

    sets up imports and exports for home page

--- app/http/web/app/src/Home/index.js
+++ app/http/web/app/src/Home/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import SwipeableViews from 'react-swipeable-views';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Grid from '@material-ui/core/Grid';
+import { withAuth } from '@okta/okta-react';
 
-const home = (props) => {
-    return (
-        <div>Home</div>
-    )
-};
+import GithubRepo from "../GithubRepo"
+import SearchBar from "../SearchBar"
 
-export default home;
+import githubClient from '../githubClient'
+import APIClient from '../apiClient'
+
+
+
+
+
+
+
+
+
+
+
+export default withStyles(styles)(withAuth(home));
