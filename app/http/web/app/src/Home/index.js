610790b6ab8a55450acab929487bc012c52e5731
Dev Govindji <friendsofdev94@gmail.com>
2019-11-04 15:52:18 -0600

    add state and state handlers

--- app/http/web/app/src/Home/index.js
+++ app/http/web/app/src/Home/index.js
@@ -12,7 +12,46 @@ import SearchBar from "../SearchBar"
 import githubClient from '../githubClient'
 import APIClient from '../apiClient'
 
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        marginTop: 30
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+});
 
+class Home extends React.Component {
+    state = {
+        value: 0,
+        repos: [],
+        kudos: []
+    };
+
+    async componentDidMount() {
+        const accessToken = await this.props.auth.getAccessToken()
+        this.apiClient = new APIClient(accessToken);
+        this.apiClient.getKudos().then((data) =>
+            this.setState({ ...this.state, kudos: data })
+        );
+    }
+
+    handleTabChange = (event, value) => {
+        this.setState({ value });
+    };
+
+    handleTabChangeIndex = index => {
+        this.setState({ value: index });
+    };
+
+    resetRepos = repos => this.setState({ ...this.state, repos })
+
+
+
+}
 
 
 
